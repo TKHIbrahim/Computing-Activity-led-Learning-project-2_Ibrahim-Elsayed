eed6966cda7e28f0c893c8dc026f1a0ecae1d2b7
TKHIbrahim <92651995+TKHIbrahim@users.noreply.github.com>
2022-03-06 23:52:14 +0200

    Update lab1_activity_1.ino

--- KH4009CEM_Ibrahim Elsayed/Week 1/lab1_activity_1/lab1_activity_1.ino
+++ KH4009CEM_Ibrahim Elsayed/Week 1/lab1_activity_1/lab1_activity_1.ino
@@ -1,28 +1,16 @@
-int a = 1 ;
+int a = 0 ;
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
-while(a<100)
-{
-  Serial.println(a);
-  if(a % 3 == 0 && a % 5 == 0)
-  {
-    Serial.println("FizzBuzz");
-  }
-  else if (a % 5 == 0)
-  {
-    Serial.println("Buzz");
-  }
-  else if(a % 3 == 0)
-  {
-    Serial.println("Fizz");
-  }
-  
-  a++;
-}
+
+
+
 }
 
 void loop(){
  
+Serial.println(a);
+a++;
+delay(1);
 
    }
